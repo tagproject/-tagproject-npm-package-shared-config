139c73b12bda6c914de887724ba86f238e7db93e
keindev <kein@tagproject.ru>
2021-05-01 03:42:33 +0300

    improve: update configs for ES6 modules

--- .config/jest.config.cjs
+++ .config/jest.config.cjs
@@ -6,8 +6,8 @@ module.exports = {
   collectCoverage: true,
   coverageReporters: ['text-summary', 'lcov'],
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/lib/', '/node_modules/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testPathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
