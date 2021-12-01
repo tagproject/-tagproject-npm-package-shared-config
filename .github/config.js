bd92b2a87944db7003e1879c2b3bb448c3fdf652
keindev <kein@tagproject.ru>
2021-12-02 01:53:02 +0300

    workflow(ci): test matrix from engines

---
+++ .github/config.js
@@ -0,0 +1,15 @@
+var semver = require('semver');
+
+const engines = '^14.13.1 || >=16.0.0';
+const comparators = semver
+  .toComparators(engines)
+  .flat()
+  .map(v => semver.coerce(v));
+const minVersion = semver.minSatisfying(comparators, engines).version;
+const maxVersion = semver.maxSatisfying(comparators, engines).version;
+
+console.log({
+  node: ['16.x'],
+});
+
+// console.log([minVersion, `${semver.major(maxVersion)}.x`]);
